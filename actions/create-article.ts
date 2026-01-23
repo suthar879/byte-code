6ba1c8150ff7fa54464d88bd5de90a7679552f87
suthar879 <anil.suthar.50746@gmail.com>
2026-01-23 10:48:48 +0530

    implement article creation with Cloudinary image upload; enhance form validation and error handling

--- actions/create-article.ts
+++ actions/create-article.ts
@@ -2,6 +2,16 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { z } from "zod";
+import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
+import { error } from "console";
+import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const createArticleSchema = z.object({
   title: z.string().min(3).max(100),
@@ -20,6 +30,7 @@ type CreateArticlesFormstate = {
 };
 
 export const createArticle = async (
+  prevState: CreateArticlesFormstate,
   formData: FormData,
 ): Promise<CreateArticlesFormstate> => {
   const result = createArticleSchema.safeParse({
@@ -38,5 +49,87 @@ export const createArticle = async (
       errors: { formsErrors: ["you have to login first"] },
     };
   }
+
+  const existingUser = await prisma.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!existingUser) {
+    return {
+      errors: {
+        formsErrors: [
+          "User not found. Please register before creating an articles",
+        ],
+      },
+    };
+  }
+
+  const imageFile = formData.get("featuredImage") as File | null;
+  if (!imageFile || imageFile.name === "undefined") {
+    return {
+      errors: {
+        featuredImage: ["Image file is required"],
+      },
+    };
+  }
+
+  const arrayBuffer = await imageFile.arrayBuffer();
+
+  const buffer = Buffer.from(arrayBuffer);
+
+  const uploadResponse: UploadApiResponse | undefined = await new Promise(
+    (resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        {
+          resource_type: "auto",
+        },
+        (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(result);
+          }
+        },
+      );
+      uploadStream.end(buffer);
+    },
+  );
+
+  const imageUrl = uploadResponse?.secure_url;
+
+  if (!imageUrl) {
+    return {
+      errors: {
+        featuredImage: ["Failed to upload image. Please try again"],
+      },
+    };
+  }
+
+  try {
+    await prisma.articles.create({
+      data: {
+        title: result.data.title,
+        category: result.data.category,
+        content: result.data.content,
+        featuredImage: imageUrl,
+        authorId: existingUser.id,
+      },
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        errors: {
+          formsErrors: [error.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          formsErrors: ["Some internal error occurred"],
+        },
+      };
+    }
+  }
+  revalidatePath("/dashboard");
   redirect("/dashboard");
 };
--- components/articles/create-articles-page.tsx
+++ components/articles/create-articles-page.tsx
@@ -1,16 +1,35 @@
 "use client";
-import React, { useState } from "react";
+import React, {
+  FormEvent,
+  startTransition,
+  useActionState,
+  useState,
+} from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import dynamic from "next/dynamic";
 import { Button } from "../ui/button";
 import "react-quill-new/dist/quill.snow.css";
+import { createArticle } from "@/actions/create-article";
 
 const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
 
 const CreateArticlesPage = () => {
   const [content, setContent] = useState("");
+  const [formState, action, isPending] = useActionState(createArticle, {
+    errors: {},
+  });
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    formData.append("content", content);
+    startTransition(() => {
+      action(formData);
+    });
+  };
+
   return (
     <div className="max-w-4xl mx-auto p-6">
       <Card>
@@ -18,18 +37,32 @@ const CreateArticlesPage = () => {
           <CardTitle>Create New Articles</CardTitle>
         </CardHeader>
         <CardContent>
-          <form className="space-y-6">
+          <form onSubmit={handleSubmit} className="space-y-6">
             <div className="space-y-2">
               <Input type="text" name="title" placeholder="Enter a article" />
+              {formState.errors.title && (
+                <span className="text-red-600 text-sm">
+                  {formState.errors.title}
+                </span>
+              )}
             </div>
             <div className="space-y-2">
               <Label>Category</Label>
-              <select className="flex h-10 w-full rounded-md">
+              <select
+                className="flex h-10 w-full rounded-md"
+                name="category"
+                id="categorysss"
+              >
                 <option value="">Select Category</option>
                 <option value="technology">Technology</option>
                 <option value="programming">Programming</option>
                 <option value="web-development">Web Development</option>
               </select>
+              {formState.errors.category && (
+                <span className="text-red-600 text-sm">
+                  {formState.errors.category}
+                </span>
+              )}
             </div>
             <div className="space-y-2">
               <Label htmlFor="featuredImage">Featured Image</Label>
@@ -43,10 +76,17 @@ const CreateArticlesPage = () => {
             <div className="space-y-2">
               <Label>Content</Label>
               <ReactQuill theme="snow" value={content} onChange={setContent} />
+              {formState.errors.content && (
+                <span className="text-red-600 text-sm">
+                  {formState.errors.content}
+                </span>
+              )}
             </div>
             <div className="flex justify-end gap-4">
               <Button variant={"outline"}>Cancel</Button>
-              <Button type="submit">Publish Article</Button>
+              <Button type="submit" disabled={isPending}>
+                {isPending ? "loading" : "Publish Article"}
+              </Button>
             </div>
           </form>
         </CardContent>
